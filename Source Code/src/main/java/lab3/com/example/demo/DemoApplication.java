e43ecef995eef6a6589ace65cf556967930cc98c
Brenden Taul <89364653+btaul@users.noreply.github.com>
2021-11-12 18:52:34 -0600

    added in login functionality

--- Source Code/src/main/java/lab3/com/example/demo/DemoApplication.java
+++ Source Code/src/main/java/lab3/com/example/demo/DemoApplication.java
@@ -2,10 +2,8 @@ package lab3.com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//note that i am disabling the login screen A&A here
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class DemoApplication {
 
     public static void main(String[] args) {
