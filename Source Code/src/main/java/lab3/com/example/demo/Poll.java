b022dde2f12f0e3e87bbed75a94d3da284562412
henry-bulluck <thbulluck@gmail.com>
2021-11-16 17:22:55 -0600

    Added Poll table and changed database

---
+++ Source Code/src/main/java/lab3/com/example/demo/Poll.java
@@ -0,0 +1,103 @@
+package lab3.com.example.demo;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "poll")
+public class Poll {
+    //Model in MVC
+
+    //PK
+    @Id
+    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long PollID;
+
+    @Column(nullable = false)
+    private Long UserID;
+
+    @Column(nullable = false, length = 45)
+    private String Title;
+
+    @Column(length = 45)
+    private String Location;
+
+    @Column(length = 45)
+    private String Notes;
+
+    @Column(nullable = false, length = 45)
+    private String TimeZone;
+
+    @Column(nullable = false)
+    private Integer VotesPer;
+
+    @Column(nullable = false, length = 45)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date Deadline;
+
+    public Long getPollID() {
+        return PollID;
+    }
+
+    public void setPollID(Long pollID) {
+        PollID = pollID;
+    }
+
+    public Long getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(Long userID) {
+        UserID = userID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+
+    public String getTimeZone() {
+        return TimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        TimeZone = timeZone;
+    }
+
+    public Integer getVotesPer() {
+        return VotesPer;
+    }
+
+    public void setVotesPer(Integer votesPer) {
+        VotesPer = votesPer;
+    }
+
+    public Date getDeadline() {
+        return Deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        Deadline = deadline;
+    }
+}
